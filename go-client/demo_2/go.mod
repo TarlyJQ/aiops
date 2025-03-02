baaaba6ea156fbbdfa4c799275caba0cdd972349
tarly <cyj_qhims@163.com>
2025-03-02 20:32:03 +0800

    new demo

---
+++ go-client/demo_2/go.mod
@@ -0,0 +1,3 @@
+module github.com/TarlyJQ/aiops/go-client/demo_2
+
+go 1.24.0
---
+++ go-client/demo_2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/clinet-go/rest"
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "/root/.kube/config", "Path to the kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		fmt.Printf("err %s", err.Error())
+	}
+	config.APIPath = "api"
+	config.GroupVersion = &corev1.SchemeGroupVersion
+	config.NegotiatedSerializer = scheme.Codecs
+
+	// 初始化 restclient
+	restClint, err := rest.RESTClientFor(config)
+	if err != nil {
+		fmt.Printf("err %s", err.Error())
+	}
+
+	// 创建一个空结构体，存储 pod 列表
+	podList := &corev1.PodList{}
+	restClint.Get().Namespace("kube-system").Resource("pods").Do(context.TODO()).Into(podList)
+
+}
